b133a9920dfd81b39697138b3be0c1b23b202994
Werner Dietl <wdietl@gmail.com>
2023-11-20 23:21:15 -0500

    Explicit imports

--- checker/tests/nullness-initialization/EisopIssue610.java
+++ checker/tests/nullness-initialization/EisopIssue610.java
@@ -1,10 +1,12 @@
-import org.checkerframework.checker.initialization.qual.*;
-import org.checkerframework.checker.nullness.qual.*;
-
-// Cover eisop issue 610:
+// Test case for EISOP issue 610:
 // https://github.com/eisop/checker-framework/issues/610
+
 // The issue was that receivers of class type `A` whose fields are all initialized were sometimes
 // considered `@UnderInitialization(A.class)` instead of `@Initialized` even when `A` was final.
+
+import org.checkerframework.checker.initialization.qual.Initialized;
+import org.checkerframework.checker.nullness.qual.MonotonicNonNull;
+import org.checkerframework.checker.nullness.qual.Nullable;
 
 final class EisopIssue610_1 {
     @MonotonicNonNull String s;
